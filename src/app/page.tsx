2130e74bfa21477493b2f1f60072440bda0770f2
MuammarRizal31 <muammarrizal31@gmail.com>
2025-05-04 23:44:07 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,6 +48,7 @@ const Home = () => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-orange-50 to-white py-10 px-4">
       <div className="mx-auto max-w-4xl">
+        <img src="/header.webp" alt="" />
         {/* Header Section */}
         <header className="mb-12 text-center">
           <h1 className="mb-4 text-4xl font-bold text-gray-800 sm:text-5xl md:text-6xl">
