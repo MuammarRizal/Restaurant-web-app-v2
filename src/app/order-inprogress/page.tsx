b302f95b71cb16e73c2d3e074b80dec4bb973848
MuammarRizal31 <muammarrizal31@gmail.com>
2025-05-04 23:40:25 +0700

    add image

--- src/app/order-inprogress/page.tsx
+++ src/app/order-inprogress/page.tsx
@@ -74,7 +74,7 @@ const OrderInProgress = () => {
           className="cursor-pointer mt-6 flex items-center justify-center gap-2 px-4 py-2 bg-orange-500 text-white rounded-lg hover:bg-orange-600 transition-colors mx-auto"
         >
           <Home className="w-5 h-5" />
-          <span>Silahkan Scan Lagi</span>
+          <span>Silahkan Scan Lagi  </span>
         </motion.button>
 
         {/* Chef Illustration */}
