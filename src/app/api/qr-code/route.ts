3e312e9b7f1169bf24097ceb537cfa2f8d14b2e9
MuammarRizal31 <muammarrizal31@gmail.com>
2025-05-18 22:44:18 +0700

    update error

--- src/app/api/qr-code/route.ts
+++ src/app/api/qr-code/route.ts
@@ -1,8 +1,8 @@
 import { addDataFirebase, retrieveData } from "@/lib/firebase/service"
 import { NextRequest, NextResponse } from "next/server"
-interface qr_code {
-    id: string;
-    code: string;
+type qr_code = {
+    id?: string;
+    code?: string;
 }
 export async function GET(request: NextRequest){
     const menus = await retrieveData("qr_code")
@@ -13,12 +13,13 @@ export async function GET(request: NextRequest){
     })
 }
 
-export async function POST(request: NextRequest,response: NextResponse){
+export async function POST(request: NextRequest){
     const body = await request.json()
     const {code} = body
-    const qr_code = await retrieveData("qr_code")
+    const qr_code: qr_code[] = await retrieveData("qr_code")
+    console.log({qr_code})
      
-    const foundQrCodes = qr_code.filter((item: any) => item.code === code);
+    const foundQrCodes = qr_code.filter((item: qr_code) => item.code === code);
     console.log({foundQrCodes})
     
     if (foundQrCodes.length > 0) {
